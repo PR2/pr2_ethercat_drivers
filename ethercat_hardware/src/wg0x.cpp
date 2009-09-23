4125cde6bd070f4bfbd80af982770045a4021eda
blaise <blaise@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-09-23 22:40:17 +0000

    Added setting of output for WG021 board.

--- ethercat_hardware/src/wg0x.cpp
+++ ethercat_hardware/src/wg0x.cpp
@@ -496,6 +496,7 @@ void WG021::packCommand(unsigned char *buffer, bool halt, bool reset)
   // Pack command structures into EtherCAT buffer
   WG021Command *c = (WG021Command *)buffer;
   memset(c, 0, command_size_);
+  c->digital_out_ = digital_out_.command_.data_;
   c->programmed_current_ = int(cmd.current_ / config_info_.nominal_current_scale_);
   c->mode_ = cmd.enable_ ? (MODE_ENABLE | MODE_CURRENT | MODE_SAFETY_RESET) : MODE_OFF;
   c->config0_ = ((cmd.A_ & 0xf) << 4) | ((cmd.B_ & 0xf) << 0);
