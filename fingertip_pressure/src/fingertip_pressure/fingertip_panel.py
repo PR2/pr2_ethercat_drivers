ac345ad2f38c8c1bf7111e13d9eb060a385683ad
blaise <blaise@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-04-07 17:33:43 +0000

    Added a request and check for version 2.8 of wx to make sure the fingertip pressure panel works.

--- fingertip_pressure/src/fingertip_pressure/fingertip_panel.py
+++ fingertip_pressure/src/fingertip_pressure/fingertip_panel.py
@@ -46,6 +46,13 @@ from fingertip_pressure.colormap import color255
 from fingertip_pressure.msg import PressureInfo
 from geometry_msgs.msg import Vector3
 
+WXVER = '2.8'
+import wxversion
+if wxversion.checkInstalled(WXVER):
+  wxversion.select(WXVER)
+else:
+  print >> sys.stderr, "This application requires wxPython version %s"%(WXVER)
+  sys.exit(1)
 import wx
 import threading
 from wx import xrc
