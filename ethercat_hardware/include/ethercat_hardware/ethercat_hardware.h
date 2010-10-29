14023e7dd296c29d0afe2eabbcfe4b52bf708d4d
dking <dking@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-10-29 06:40:17 +0000

    Reverting default timeout to 20ms, and retries to 5. Added timeout and retry values to diagnostics.

--- ethercat_hardware/include/ethercat_hardware/ethercat_hardware.h
+++ ethercat_hardware/include/ethercat_hardware/ethercat_hardware.h
@@ -113,7 +113,8 @@ public:
    * \param buffer_size size of proccess data buffer
    * \param number of EtherCAT slave devices
    */
-  void initialize(const string &interface, unsigned int buffer_size, EthercatDevice **slaves, unsigned int num_slaves);
+  void initialize(const string &interface, unsigned int buffer_size, EthercatDevice **slaves, unsigned int num_slaves,
+                  unsigned timeout, unsigned max_pd_retries);
 
   /*!
    * \brief Triggers publishing of new diagnostics data
@@ -173,6 +174,9 @@ private:
   EthercatDevice **slaves_;
   unsigned int num_slaves_;
   string interface_;
+
+  unsigned timeout_;
+  unsigned max_pd_retries_;
 
   //! Count of dropped packets last diagnostics cycle
   uint64_t last_dropped_packet_count_;
@@ -262,6 +266,7 @@ private:
   bool halt_motors_;
   unsigned int reset_state_;
 
+  unsigned timeout_;        //!< Timeout (in microseconds) to used for sending/recieving packets once in realtime mode.
   unsigned max_pd_retries_; //!< Max number of times to retry sending process data before halting motors
 
   void publishDiagnostics();  //!< Collects raw diagnostics data and passes it to diagnostics_publisher
