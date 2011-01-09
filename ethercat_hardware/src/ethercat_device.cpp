731abc4c5020ae7578b60dd76c251f4eb1365543
dking <dking@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2011-01-09 08:25:46 +0000

    Add delay before exit so that rosoutput has time to get published to network.

--- ethercat_hardware/src/ethercat_device.cpp
+++ ethercat_hardware/src/ethercat_device.cpp
@@ -336,12 +336,14 @@ void EthercatDevice::construct(EtherCAT_SlaveHandler *sh, int &start_address)
   int error = pthread_mutex_init(&newDiagnosticsIndexLock_, NULL);
   if (error != 0) {
     ROS_FATAL("Initializing indexLock failed : %s", strerror(error));
+    sleep(1); // wait for ros to flush rosconsole output
     exit(EXIT_FAILURE);
   }
 
   error = pthread_mutex_init(&diagnosticsLock_, NULL);
   if (error != 0) {
     ROS_FATAL("Initializing diagnositcsLock failed : %s", strerror(error));
+    sleep(1); // wait for ros to flush rosconsole output
     exit(EXIT_FAILURE);
   }    
 }
